40005d0148afe0875aa0549b24f1118d8bd5774a
lladdy <randallboland@bigpond.com>
2018-08-04 22:17:34 +0930

    Hide invisible bot types from users

--- app/controllers/bot_types_controller.rb
+++ app/controllers/bot_types_controller.rb
@@ -1,5 +1,12 @@
 class BotTypesController < ApplicationController
   def index
-    render json: BotType.all
+    authorize! :read, BotType
+    if current_user.present? && current_user.role == "admin" # is it bad to hardcode this?
+      render json: BotType.all
+    else
+      # todo: this returns nil if nothing exists
+      # todo: I'd prefer it returned an empty JSON object
+      render json: BotType.find_by(invisible: false)
+    end
   end
 end
